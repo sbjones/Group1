6cacdfd463efc6e2127ae4089e95dfb2359c4a1c
unknown <cpfeger2@DHE167-12.secs.oakland.edu>
2013-04-04 13:40:39 -0400

    some small changes

--- conn.inc.php
+++ conn.inc.php
@@ -1,4 +1,4 @@
 <?php
-$conn=mysql_connect("localhost", "root", "") or die(mysql_error());
+$conn=mysql_connect("localhost", "cpfeger2", "george10") or die(mysql_error());
 $db = mysql_select_db("cpfeger2_...") or die(mysql_error());
 ?>
